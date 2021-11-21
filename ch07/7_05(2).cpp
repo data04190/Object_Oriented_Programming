7e2c3c70de43c674b246182dd66933d30309f292
data04190 <77683645+data04190@users.noreply.github.com>
2021-11-21 23:38:08 +0900

    Add files via upload

---
+++ ch07/7_05(2).cpp
@@ -0,0 +1,41 @@
+//책 제목 사전 순으로 비교 (< 연산자 작성)
+
+#include<iostream>
+using namespace std;
+
+class Color {
+	int red, green, blue;
+public:
+	Color() { red = green = blue = 0; }
+	Color(int r, int g, int b) { red = r; green = g; blue = b; }
+	void setColor(int r, int g, int b) { red = r; green = g; blue = b; }
+	void show(){ cout << red << ' ' << green << ' ' << blue << endl; }
+	friend Color operator+ (Color c1, Color c2);
+	friend bool operator==(Color c1, Color c2);
+};
+
+Color operator+(Color c1, Color c2) {
+	Color c;
+	c.red = c1.red + c2.red;
+	c.green = c1.green + c2.green;
+	c.blue = c1.blue + c2.blue;
+	return c;
+}
+bool operator==(Color c1, Color c2) {
+	if (c1.red == c2.red && c1.green == c2.green && c1.blue == c2.blue)
+		return true;
+	else false;
+}
+
+int main() {
+
+	Color red(255, 0, 0), blue(0, 0, 255), c;
+	c = red + blue;
+	c.show();
+
+	Color fuchsia(255, 0, 255);
+	if (c == fuchsia)
+		cout << "보라색 맞음";
+	else
+		cout << "보라색 아님";
+}
