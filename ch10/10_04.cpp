74bb1190bdc2845a92c8f95c157aa82f5d8eced7
data04190 <77683645+data04190@users.noreply.github.com>
2021-12-11 00:39:39 +0900

    Add files via upload

---
+++ ch10/10_04.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+template <class T1>
+bool search(T1 s,T1 x[],int n) {
+	for (int i = 0; i < n; i++) {
+		if (x[i] == s)
+			return true;
+	}
+	return false;
+}
+
+int main() {
+	int x[] = { 1,10,100,5,4 };
+	if (search(100, x, 5)) cout << "100이 배열 x에 포함되어 있다";
+	else cout << "100이 배영ㄹ x에 포함되어 있지 않다";
+	
+}
