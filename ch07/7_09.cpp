e79a6dc8e3038664096cfa42fd32d9f76a0b7464
data04190 <77683645+data04190@users.noreply.github.com>
2021-12-19 01:16:50 +0900

    Add files via upload

---
+++ ch07/7_09.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+class Circle {
+	int radius;
+public:
+	Circle(int radius = 0) { this->radius = radius; }
+	void show() { cout << "radius = " << radius << " ÀÎ ¿ø" << endl; }
+	friend Circle operator + (int n, Circle a);
+};
+Circle operator +(int n, Circle a) {
+	a.radius = n + a.radius;
+	return a;
+}
+int main() {
+	Circle a(5), b(4);
+	b = 1 + a;
+	a.show();
+	b.show();
+}
